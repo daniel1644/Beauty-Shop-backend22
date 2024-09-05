8a6f84406495375dd90957d0935d62bc25698f71
daniel beyene <dazimore@gmail.com>
2024-09-06 01:04:02 +0300

    update finalize

--- app/schemas.py
+++ app/schemas.py
@@ -15,6 +15,7 @@ class UserSchema(Schema):
 class ProductSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True, validate=validate.Length(min=1, max=120))
+    image = fields.Str(required=True, validate=validate.Length(min=1, max=120))
     price = fields.Float(required=True, validate=validate.Range(min=0.01))
     description = fields.Str(validate=validate.Length(max=500))
     category = fields.Str(required=True, validate=validate.Length(max=50))
--- seed.py
+++ seed.py
@@ -19,14 +19,14 @@ def seed_data():
     accessories = [
         {'name': 'Hair rollers', 'image': 'https://i.pinimg.com/originals/94/26/53/9426534a94ef632cf5762f27b69bf985.jpg', 'category': 'Accessories', 'price': 10, 'description': 'Essential for perfect curls', 'stock': 100},
         {'name': 'Skin toner', 'image': 'https://i.pinimg.com/1200x/ca/7c/ae/ca7cae9f7e9f4fe512172b5bbd8331c8.jpg', 'category': 'Accessories', 'price': 15, 'description': 'Refreshes and balances skin', 'stock': 150},
-        {'name': 'Moisturizing face mask', 'image': 'https://i.pinimg.com/originals/5d/c7/07/5dc707af23b776dd99336b36e4c4d21f.jpg', 'category': 'Accessories', 'price': 20, 'description': 'Hydrates and revitalizes skin', 'stock': 200},
-        {'name': 'Nail polish remover', 'image': 'https://i.pinimg.com/originals/70/d5/d9/70d5d93a8debdcc4d6e91a5e46060b93.jpg', 'category': 'Accessories', 'price': 5, 'description': 'Removes nail polish effortlessly', 'stock': 250},
-        {'name': 'Lip balm', 'image': 'https://i.pinimg.com/originals/69/70/4b/69704b2df6f8a87c00406b6b7349ea16.jpg', 'category': 'Accessories', 'price': 3, 'description': 'Keeps lips soft and hydrated', 'stock': 300},
-        {'name': 'Eyebrow pencil', 'image': 'https://i.pinimg.com/originals/ce/2e/26/ce2e26c7401a67b440ebd8e23c742081.jpg', 'category': 'Accessories', 'price': 8, 'description': 'Defines and shapes eyebrows', 'stock': 120},
-        {'name': 'Facial cleanser', 'image': 'https://i.pinimg.com/originals/d0/37/c0/d037c0a2679a5c53b8d2b0d39193c36d.jpg', 'category': 'Accessories', 'price': 12, 'description': 'Gently cleanses and purifies skin', 'stock': 180},
-        {'name': 'Hand cream', 'image': 'https://i.pinimg.com/originals/54/9e/9e/549e9e7f0ac8825dc8e9b99a5cbbd482.jpg', 'category': 'Accessories', 'price': 7, 'description': 'Nourishes and softens hands', 'stock': 220},
-        {'name': 'Shampoo brush', 'image': 'https://i.pinimg.com/originals/1c/47/c1/1c47c1648b7fcaf5a6d0fdad8d674f67.jpg', 'category': 'Accessories', 'price': 6, 'description': 'Enhances shampooing experience', 'stock': 130},
-        {'name': 'Face roller', 'image': 'https://i.pinimg.com/originals/ae/7b/6c/ae7b6c740dce6c5507d4d9fa5d4998e7.jpg', 'category': 'Accessories', 'price': 18, 'description': 'Improves circulation and reduces puffiness', 'stock': 90}
+        {'name': 'Moisturizing face mask', 'image': 'https://thebioaqua.com/cdn/shop/products/7_8e9b4b41-2499-460a-b183-4d482ef06f0d.jpg?v=1661831435&width=1445', 'category': 'Accessories', 'price': 20, 'description': 'Hydrates and revitalizes skin', 'stock': 200},
+        {'name': 'Nail polish remover', 'image': 'https://goldengirlcosmetics.com/cdn/shop/products/NailEnamelRemover-60ml-Front.jpg?v=1693328930', 'category': 'Accessories', 'price': 5, 'description': 'Removes nail polish effortlessly', 'stock': 250},
+        {'name': 'Lip balm', 'image': 'https://www.muastore.co.uk/cdn/shop/files/Hydra-Juice-Peptide-Lip-Balm-Berry-Bliss-LID-OFF_1800x1800.jpg?v=1707720522', 'category': 'Accessories', 'price': 3, 'description': 'Keeps lips soft and hydrated', 'stock': 300},
+        {'name': 'Eyebrow pencil', 'image': 'https://media.allure.com/photos/6279456fcf77efb74699598f/3:2/w_3000,h_2000,c_limit/5-09%20brow%20pencils.jpg', 'category': 'Accessories', 'price': 8, 'description': 'Defines and shapes eyebrows', 'stock': 120},
+        {'name': 'Facial cleanser', 'image': 'https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/61/7796771/1.jpg?8908', 'category': 'Accessories', 'price': 12, 'description': 'Gently cleanses and purifies skin', 'stock': 180},
+        {'name': 'Hand cream', 'image': 'https://cinnabargreen.com/app/uploads/2017/09/HANDCREAM.50ml.jpg', 'category': 'Accessories', 'price': 7, 'description': 'Nourishes and softens hands', 'stock': 220},
+        {'name': 'Shampoo brush', 'image': 'https://cdn.shopify.com/s/files/1/0243/8817/3888/products/ShampooBrush_b2822665-3f1f-4a59-8d5d-c4ca6328051c.jpg?v=1644832303', 'category': 'Accessories', 'price': 6, 'description': 'Enhances shampooing experience', 'stock': 130},
+        {'name': 'Face roller', 'image': 'https://bopowomen.com/cdn/shop/products/Rose-Quartz-Crystal-Facial-Roller.jpg?v=1653266208', 'category': 'Accessories', 'price': 18, 'description': 'Improves circulation and reduces puffiness', 'stock': 90}
     ]
     
     for accessory in accessories:
@@ -66,9 +66,9 @@ with app.app_context():
     print("Creating products...")
     products = []
     image_paths = [
-        "https://example.com/image1.jpg",
-        "https://example.com/image2.jpg",
-        "https://example.com/image3.jpg",
+        "https://media-cldnry.s-nbcnews.com/image/upload/rockcms/2024-06/240610-beauty-awards-2024-face-makeup-winners-vl-social-74fb90.jpg",
+        "https://www.rarebeauty.com/cdn/shop/files/gnav-shop-dropdown-body-400x400_400x.jpg?v=1702303892",
+        "https://assets.beautyhub.co.ke/wp-content/uploads/2024/03/29151351/beauty_of_joseon_glow_set.jpg",
         # Add more image URLs here
     ]
     for _ in range(20):
